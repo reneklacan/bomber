5020926d0570ffe9af5f35ce480dd4bb32368fad
Rene Klacan <simpliplant@gmail.com>
2013-06-07 01:21:13 +0200

    working android project setup

---
+++ client/src/ProjAndroid/jni/Android.mk
@@ -0,0 +1,29 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := hellocpp_shared
+
+LOCAL_MODULE_FILENAME := libhellocpp
+
+LOCAL_SRC_FILES := hellocpp/main.cpp \
+        ../../Classes/AppDelegate.cpp \
+        ../../Classes/GameplayScene.cpp \
+        ../../Classes/GameOverScene.cpp \
+        ../../Classes/Input/Joystick.cpp \
+        ../../Classes/Input/Keyboard.cpp \
+        ../../Classes/Input/ControlLayer.cpp \
+        ../../Classes/Gameplay/Map/Map.cpp \
+        ../../Classes/Gameplay/Map/Bomb.cpp \
+        ../../Classes/Gameplay/Map/Explosion.cpp \
+        ../../Classes/Gameplay/Sprites/GameSprite.cpp \
+        ../../Classes/Gameplay/Sprites/Human.cpp \
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+
+LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
+
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,cocos2dx)
+
