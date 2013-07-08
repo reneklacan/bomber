db7acacfa2cb6f0e0552179b556495f897f2882d
Rene Klacan <simpliplant@gmail.com>
2013-07-08 16:33:48 +0200

    vystup na konzolu vo windowse

--- client/src/ProjWindows/Main.cpp
+++ client/src/ProjWindows/Main.cpp
@@ -1,15 +1,41 @@
+#include "io.h"
+#include "fcntl.h"
+
 #include "Main.h"
 #include "../Classes/AppDelegate.h"
-//#include "cocos2d.h"
-
 #include "CCEGLView.h"
 
-//#include <stdlib.h>
-//#include <stdio.h>
-//#include <unistd.h>
-//#include <string>
-
 USING_NS_CC;
+
+static void openConsole()
+{
+    int outHandle, errHandle, inHandle;
+    FILE *outFile, *errFile, *inFile;
+    AllocConsole();
+    CONSOLE_SCREEN_BUFFER_INFO coninfo;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &coninfo);
+    coninfo.dwSize.Y = 9999;
+    SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE), coninfo.dwSize);
+
+    outHandle = _open_osfhandle((long)GetStdHandle(STD_OUTPUT_HANDLE), _O_TEXT);
+    errHandle = _open_osfhandle((long)GetStdHandle(STD_ERROR_HANDLE),_O_TEXT);
+    inHandle = _open_osfhandle((long)GetStdHandle(STD_INPUT_HANDLE),_O_TEXT );
+
+    outFile = _fdopen(outHandle, "w" );
+    errFile = _fdopen(errHandle, "w");
+    inFile =  _fdopen(inHandle, "r");
+
+    *stdout = *outFile;
+    *stderr = *errFile;
+    *stdin = *inFile;
+
+    setvbuf( stdout, NULL, _IONBF, 0 );
+    setvbuf( stderr, NULL, _IONBF, 0 );
+    setvbuf( stdin, NULL, _IONBF, 0 );
+
+    std::ios::sync_with_stdio();
+
+}
 
 int APIENTRY _tWinMain(HINSTANCE hInstance,
                        HINSTANCE hPrevInstance,
@@ -18,7 +44,9 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
-    // create the application instance
+
+    openConsole();
+
     AppDelegate app;
 
     EGLView* eglView = EGLView::sharedOpenGLView();
